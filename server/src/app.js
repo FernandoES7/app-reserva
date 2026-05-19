6b1b2decce00bd80d566acdaca7e3b9e770375fe
Fernando <>
2026-05-18 22:09:56 -0500

    Prueba conexión a db

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import cors from'cors';
+import cors from 'cors';
+import pool from './database/connection.js';
 
 const app = express();
 
@@ -10,6 +11,21 @@ app.get('/', (req, res) => {
   res.json({
     message: 'API funcionando'
   });
+});
+
+app.get('/test-db', async (req, res) => {
+  try{
+    const [rows] = await pool.query('SELECT NOW() AS currentDate');
+    res.json({
+      ok: true,
+      data:rows
+    });
+  }catch(error){
+    res.status(500).json({
+      ok:false,
+      error:error.message
+    });
+  }
 });
 
 export default app;
